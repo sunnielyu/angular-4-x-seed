4157253af6c0efa94c7e795ecfbdd853d94e7ce6
Josh Hills <josh.hills@ymail.com>
2017-10-28 22:56:12 +0100

    Cleaning boilerplate.

--- source/scripts/app.module.ts
+++ source/scripts/app.module.ts
@@ -3,26 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from 
-
-const appRoutes: Routes = [
-  {
-    path: 'pattern',
-    component: PatternComponent
-  }
-];
-
 @NgModule({
   imports: [
-    // Enable routing.
-    RouterModule.forRoot(
-      appRoutes
-    ),
     BrowserModule,
     FormsModule
   ],
   declarations: [
-    PatternComponent
   ],
   bootstrap: [ ]
 })
